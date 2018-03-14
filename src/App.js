e2c0b73aabddd52b95cf20fd7351dd5f0ac14d1f
Scott M Parrish <anithri@gmail.com>
2018-03-14 17:33:50 -0600

    basic store functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider} from 'unstated';
+import Store from './store';
+import Counter from './Counter';
+
 const App = () => {
   return (
-    <div>
-    <p>React here!</p>
-  </div>
-);
+    <Provider inject={Store}>
+      <Counter />
+      <p>React here!</p>
+      <Counter/>
+    </Provider>
+  );
 };
+
 export default App;
 ReactDOM.render(<App />, document.getElementById("app"));
+
