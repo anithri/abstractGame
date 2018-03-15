330f408380ee30976ed1a6dbbe94f3442c5d36c5
Scott M Parrish <anithri@gmail.com>
2018-03-14 22:25:59 -0600

    PlayerRegion styles and structure

--- src/panes/PlayerRegion/index.js
+++ src/panes/PlayerRegion/index.js
@@ -1,23 +1,31 @@
 import cx from 'classnames';
 import React from "react";
 import PropTypes from "prop-types";
-import styles from "./PlayerRegion.css";
+import styles from "./playerRegion.css";
 import Players from "models/Players";
 
 class PlayerRegion extends React.Component {
 
   render () {
     const player = Players.find(this.props.playerId);
+    const game = {};
     const classNames = cx(
       styles.playerRegion,
       this.props.className, this.props.theme
     );
 
     return (
-      <div className={classNames} >
-        <h3>{player.name}</h3>
-      </div>
-    )
+      <section className={classNames}>
+        <header>
+          <h3 className={styles.name}>{player.name}</h3>
+          <h3 >{game.score || 3}</h3>
+          <span>{player.spheres[0]}</span>
+          <span>{player.spheres[1]}</span>
+          <span>{player.spheres[2]}</span>
+        </header>
+        <main className={styles.main}>There will be stuff here!</main>
+      </section>
+   )
   }
 }
 
